82a8f4ff2cec4c2068af7522f395a3457e57b0de
David M. Cruz <114960658+DavidMeDEV@users.noreply.github.com>
2026-01-28 20:22:32 -0300

    Adição da função de busca no dicionario

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+    "F": "respeito",
+    "FF": "desistir",
+    "GG": "bom jogo",
+    "RUSHAR":"ir pra cima",
+    "BROCKEN":"algo que esta desbalanceado"
+}
+
+word = input("digite uma palavra que esta dentro do nosso dicionário e direi oseu significado:")
+
+if word in meme_dict.keys():
+    print (meme_dict[word])
+else:
+    print("esta palavra não conta no nosso dicionário")
